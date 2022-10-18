c5e68728ae76fe74a0f274521640fd0087cf28db
student <evelyns60@nycstudents.net>
2022-10-18 10:57:00 -0400

    10/18/22 class push

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,5 +1,5 @@
 public class LinearEquation {
-    private int x1;
+    private int  x1;
     private int y1;
     private int x2;
     private int y2;
@@ -27,8 +27,39 @@ public class LinearEquation {
         return roundedToHundredth(yIntercept);
     }
 
+    public String equation() {
+
+
+        if (((double)(y2) - y1)/(x2-x1) % 1 == 0) {        //is it a whole number
+            if (yIntercept() == 0) {                       //is y intercept zero
+                return "y = " + slope() + "x";
+            } else {                                       //is y intercept not zero
+                return "y = " + slope() + "x + " + yIntercept();
+            }
+        } else {                                          //is it a fraction
+            int rise = y2 - y1;
+            int run = x2 - x1;
+            String fractionSlope = rise + "/" + run;
+            return "y = " + fractionSlope +  "x + " + yIntercept();
+        }
+
+    }
+
+    public String coordinateForX(double xValue) {
+        double yValue = slope() * xValue + yIntercept();
+        return "(" + xValue + ", " + yValue + ")";
+    }
+
+    public String lineInfo() {
+        String str = "The two  points are: (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ")";
+        str += "\nThe equation of the line between these points is: " + equation();
+        str += "\nThe slope of this line is: " + slope();
+        str += "\nThe y-intercept of this line is: " + yIntercept();
+        str += "\nThe distance between the two points is: " + distance();
+        return str;
+    }
+
     public double roundedToHundredth(double toRound) {
-        double rounded = Math.round(toRound*100) / 100.0;
-        return rounded;
+        return Math.round(toRound*100) / 100.0;
     }
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -5,22 +5,31 @@ public class LinearEquationRunner {
         System.out.println("Welcome!");
         System.out.println("Enter coordinate 1: ");
         String coor1 = scan.nextLine();
-        String x1 = coor1.substring(1,coor1.indexOf(","));
-        String y1 = coor1.substring(coor1.indexOf(" ") + 1,coor1.length() - 1);
+        String x1AsString = coor1.substring(1,coor1.indexOf(","));
+        String y1AsString = coor1.substring(coor1.indexOf(" ") + 1,coor1.length() - 1);
+        int x1 = Integer.parseInt(x1AsString);
+        int y1 = Integer.parseInt(y1AsString);
 
         System.out.println("Enter coordinate 2: ");
         String coor2 = scan.nextLine();
-        String x2 = coor2.substring(1,coor2.indexOf(","));
-        String y2 = coor2.substring(coor2.indexOf(" ") + 1,coor2.length() - 1);
+        String x2AsString = coor2.substring(1,coor2.indexOf(","));
+        String y2AsString = coor2.substring(coor2.indexOf(" ") + 1,coor2.length() - 1);
+        int x2 = Integer.parseInt(x2AsString);
+        int y2 = Integer.parseInt(y2AsString);
+
+
+        if (x1 == x2) {
+            System.out.println("These points are on a vertical line. x = " + x1);
+            System.exit(0);
+        }
+
+        LinearEquation equation = new LinearEquation(x1, y1, x2, y2);
+        System.out.println(equation.lineInfo());
 
         System.out.println("Enter a value for x: ");
-        int x3 = scan.nextInt();
+        double x3 = scan.nextDouble();
         scan.nextLine();
-
-     /*   LinearEquation test = new LinearEquation(-1,5,3,10);
-        System.out.println(test.distance());
-        System.out.println(test.slope());
-        System.out.println(test.yIntercept()); */
+        System.out.println("The point on the line is " + equation.coordinateForX(x3));
 
     }
 }
