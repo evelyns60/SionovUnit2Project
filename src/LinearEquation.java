4089a65590fd585f5267f507ab87227aedd661bc
Evelyn Sionov <evesionov@gmail.com>
2022-10-17 22:10:53 -0400

    10/17/22 home push

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -14,8 +14,21 @@ public class LinearEquation {
 
     public double distance() {
         double distance = 	Math.sqrt(Math.pow((double)x2 - x1, 2) + Math.pow((double)y2 - y1, 2));
-        return distance;
+        return roundedToHundredth(distance);
     }
 
+    public double slope() {
+        double slope = ((double)(y2) - y1)/(x2-x1);
+        return roundedToHundredth(slope);
+    }
 
+    public double yIntercept() {
+        double yIntercept = y2 - (slope() * x2);
+        return roundedToHundredth(yIntercept);
+    }
+
+    public double roundedToHundredth(double toRound) {
+        double rounded = Math.round(toRound*100) / 100.0;
+        return rounded;
+    }
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
 public class LinearEquationRunner {
     public static void main (String[]args) {
-        LinearEquation test = new LinearEquation(-1,5,3,10);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome!");
+        System.out.println("Enter coordinate 1: ");
+        String coor1 = scan.nextLine();
+        String x1 = coor1.substring(1,coor1.indexOf(","));
+        String y1 = coor1.substring(coor1.indexOf(" ") + 1,coor1.length() - 1);
+
+        System.out.println("Enter coordinate 2: ");
+        String coor2 = scan.nextLine();
+        String x2 = coor2.substring(1,coor2.indexOf(","));
+        String y2 = coor2.substring(coor2.indexOf(" ") + 1,coor2.length() - 1);
+
+        System.out.println("Enter a value for x: ");
+        int x3 = scan.nextInt();
+        scan.nextLine();
+
+     /*   LinearEquation test = new LinearEquation(-1,5,3,10);
         System.out.println(test.distance());
+        System.out.println(test.slope());
+        System.out.println(test.yIntercept()); */
+
     }
 }
